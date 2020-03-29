9b691e6c33ab176bb7ffd47d07fc317bb84e352b
ccans <55026208+ccans@users.noreply.github.com>
2020-03-29 15:25:46 -0500

    Epic Drop Down

--- main/scripts/default.js
+++ main/scripts/default.js
@@ -2,11 +2,19 @@ function showMessage() {
   window.alert("we dont want to talk to you. ");
 }
 
+var droppedDown = false;
+
 function dropDown() {
   var el = $('.header');
+  if(droppedDown == false) {
           curHeight = "48px";
-          autoHeight = '100%';
-
-      $('.header').height(curHeight).animate({height: autoHeight}, 1000);
-      // el.height(curHeight).animate({height: autoHeight}, 1000);
+          autoHeight = '100vh';
+        $('.header').height(curHeight).animate({height: autoHeight}, 1000);
+        droppedDown = true;
+    } else {
+      curHeight = "100vh";
+      autoHeight = '48px';
+    $('.header').height(curHeight).animate({height: autoHeight}, 1000);
+    droppedDown = false;
+    }
 }
