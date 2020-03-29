dfb31558fd83cc63886fc47ae0cfe6144aaeda99
ccans <55026208+ccans@users.noreply.github.com>
2020-03-29 14:58:00 -0500

    Incomplete

--- main/scripts/default.js
+++ main/scripts/default.js
@@ -1,3 +1,12 @@
 function showMessage() {
   window.alert("we dont want to talk to you. ");
 }
+
+function dropDown() {
+  var el = $('.header');
+          curHeight = "48px";
+          autoHeight = '100%';
+
+      $('.header').height(curHeight).animate({height: autoHeight}, 1000);
+      // el.height(curHeight).animate({height: autoHeight}, 1000);
+}
