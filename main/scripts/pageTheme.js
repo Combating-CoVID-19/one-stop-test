5377fbf1d8ff822acc045dd4c1549d3fe3bd0626
DhairyaG23 <dhairyag23@gmail.com>
2020-03-29 16:12:22 -0500

    automatically change to your system setting if opted in

--- main/scripts/pageTheme.js
+++ main/scripts/pageTheme.js
@@ -1,6 +1,8 @@
 var currentTheme = localStorage.getItem('page-theme');
+var autoTheme = localStorage.getItem('auto-page-theme');
 const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
 function checkTheme() {
+    if(autoTheme == null){
     if(currentTheme == null && currentTheme !== "dark" && currentTheme !== "light"){
         currentTheme = localStorage.getItem('page-theme');
         if(mediaQueryList.matches){
@@ -17,6 +19,15 @@ function checkTheme() {
             document.documentElement.setAttribute('page-theme', "light")
         }
     }
+} else {
+    if(mediaQueryList.matches){
+        localStorage.setItem('page-theme', "dark")
+        document.documentElement.setAttribute('page-theme', 'dark')
+    } else {
+        localStorage.setItem('page-theme', "light");
+        document.documentElement.setAttribute('page-theme', "light")
+    }
+}
 }
 
 
@@ -24,12 +35,29 @@ function changeTheme(){
     if(currentTheme == "light"){
         currentTheme = "dark"
         localStorage.setItem('page-theme', "dark")
-        checkTheme();
+        document.documentElement.setAttribute('page-theme', "dark")
+
     } else {
         currentTheme = "light"
         localStorage.setItem('page-theme', "light")
-        checkTheme();
+        document.documentElement.setAttribute('page-theme', "light")
+
     }
 }
+
+function handleSchemeChange(){
+    autoTheme = localStorage.getItem('auto-page-theme')
+    if(autoTheme !== null){
+        if(mediaQueryList.matches){
+            localStorage.setItem('page-theme', "dark")
+            document.documentElement.setAttribute('page-theme', 'dark')
+        } else {
+            localStorage.setItem('page-theme', "light");
+            document.documentElement.setAttribute('page-theme', "light")
+        }
+    }
+}
+
+mediaQueryList.addListener(handleSchemeChange)
 
 checkTheme();
