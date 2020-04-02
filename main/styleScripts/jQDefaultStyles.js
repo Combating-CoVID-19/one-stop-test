7c5796e09bdea35826ee76bc5dde421fb513b160
ccans <55026208+ccans@users.noreply.github.com>
2020-04-02 18:42:01 -0500

    epic

--- main/styleScripts/jQDefaultStyles.js
+++ main/styleScripts/jQDefaultStyles.js
@@ -7,11 +7,37 @@ $(function() {
            $(".header").removeClass("active");
         }
 
-        if($(window).scrollTop() > $(".better").height()*.4) {
-          $(".txt").fadeIn(1000);
-          $(".imp-btn-2").fadeIn(1000);
-            // $(".txt").hide();
+        if($(window).scrollTop() > $(".better").height()*.5) {
+
+          $(".txt-1").animate({opacity: 1}, 500);
+          $(".blurb-1").animate({opacity: 1}, 500);
+          $(".imp-btn-1").animate({opacity: 1}, 500);
           }
+          if($(window).scrollTop() > $(".better").height()*.7) {
+              $(".img-elements-cntnr").hide();
+            } else {
+              $(".img-elements-cntnr").show();
+            }
+          if($(window).scrollTop() > $(".better").height()*1.3) {
+            $(".txt-2").animate({opacity: 1}, 500);
+            $(".blurb-2").animate({opacity: 1}, 500);
+            $(".imp-btn-2").animate({opacity: 1}, 500);
+            }
+            if($(window).scrollTop() > $(".better").height()*2.2) {
+              $(".txt-3").animate({opacity: 1}, 500);
+              $(".blurb-3").animate({opacity: 1}, 500);
+              $(".imp-btn-3").animate({opacity: 1}, 500);
+              }
+              if($(window).scrollTop() > $(".better").height()*3) {
+                $(".txt-4").animate({opacity: 1}, 500);
+                $(".blurb-4").animate({opacity: 1}, 500);
+                $(".imp-btn-4").animate({opacity: 1}, 500);
+                }
+                if($(window).scrollTop() > $(".better").height()*3.4) {
+                  $(".txt-5").animate({opacity: 1}, 500);
+                  $(".blurb-5").animate({opacity: 1}, 500);
+                  $(".imp-btn-5").animate({opacity: 1}, 500);
+                  }
 
     });
 });
