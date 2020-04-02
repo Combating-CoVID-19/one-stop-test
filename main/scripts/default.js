43361e6f657b7f7158b92875d3f7f8ac932b8523
ccans <55026208+ccans@users.noreply.github.com>
2020-04-02 08:45:32 -0500

    big big big RESET

--- main/scripts/default.js
+++ main/scripts/default.js
@@ -6,20 +6,20 @@ var droppedDown = false;
 
 function dropDown() {
   if(droppedDown == false) {
-        $('.dropDown-info').stop();
-          curHeight =  $('.dropDown-info').height();
+        $('.dropdown-navbar').stop();
+          curHeight =  $('.dropdown-navbar').height();
           autoHeight = $(window).height(); - $('.header').height();
-        $('.dropDown-info').height(curHeight).animate({height: autoHeight}, 750);
+        $('.dropdown-navbar').height(curHeight).animate({height: autoHeight}, 750);
         $('body').addClass("unscroll");
-        $('.dropDown-info').show();
+        $('.dropdown-navbar').show();
         $('.dropdown-content').css("display", "block");
         $('.header').addClass('dropdown');
         droppedDown = true;
     } else {
-      $('.dropDown-info').stop();
-      curHeight = $('.dropDown-info').height();
+      $('.dropdown-navbar').stop();
+      curHeight = $('.dropdown-navbar').height();
       autoHeight = '0px';
-      $('.dropDown-info').height(curHeight).animate({height: autoHeight}, 750);
+      $('.dropdown-navbar').height(curHeight).animate({height: autoHeight}, 750);
       $('body').removeClass("unscroll");
       setTimeout(function(){
       $('.dropdown-content').css("display", "none");
