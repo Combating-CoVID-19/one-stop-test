ed444b66b5395d043ff202fdf42a2db2d2fdfb81
DhairyaG23 <dhairyag23@gmail.com>
2020-04-03 17:16:11 -0500

    working chart

--- main/scripts/tracker.js
+++ main/scripts/tracker.js
@@ -2,6 +2,7 @@ var db = firebase.database();
 
 var content = document.getElementById('tracker-content')
 var countries = []
+var confirmedArray = []
 var retrievedData;
 var currentCountry;
 var x=0;
@@ -54,9 +55,14 @@ function addData(){
             // newCountryElement.innerHTML = retrievedData[currentCountry].Confirmed;
             // content.appendChild(list);
             list.appendChild(newCountryElement)
+            confirmedArray.push(retrievedData[countries[x]].Confirmed)
+
+
         }
         x++
         addData()
+        } else {
+            createChart()
         }
  
         // for (var key in retrievedData) {
@@ -74,4 +80,43 @@ function addData(){
         //        }
         //     }
         // }
+}
+function createChart() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: countries,
+            datasets: [{
+                label: '# of Votes',
+                data: confirmedArray,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
