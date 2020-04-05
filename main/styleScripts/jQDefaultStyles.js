be329f1efb3a336bdbb8555b5458ada36e1217dd
ccans <55026208+ccans@users.noreply.github.com>
2020-04-04 22:40:50 -0500

    ok

--- main/styleScripts/jQDefaultStyles.js
+++ main/styleScripts/jQDefaultStyles.js
@@ -2,12 +2,21 @@ $(function() {
     $(window).on("scroll", function() {
         if($(window).scrollTop() > 20) {
             $(".header").addClass("active");
+
         } else {
             //remove the background property so it comes transparent again (defined in your css)
            $(".header").removeClass("active");
+
         }
 
 
 
     });
+
 });
+
+window.onload = function(){
+  if($( window ).width() < 767) {
+    $(".bogo").attr("src", "/main/photos/Pandemicalm/g3667.png");
+  }
+}
