46ca967a8f9bed7fc024c4125e07ae39bfa8980e
ccans <55026208+ccans@users.noreply.github.com>
2020-04-06 18:54:27 -0500

    uhhh interesting animation to say the least

--- main/styleScripts/jQAppStyles.js
+++ main/styleScripts/jQAppStyles.js
@@ -1,44 +1,54 @@
 window.onload = (event) => {
-      $("#a1").animate({opacity: 1}, 500);
+      $("#a1").animate({opacity: 1}, 250);
       setTimeout(function(){
-      $("#a2").animate({opacity: 1}, 500);
+      $("#a2").animate({opacity: 1}, 250);
     }, 750);
       setTimeout(function(){
       $("#a3").animate({opacity: 1}, 750);
-    }, 1500);
+    }, 1250);
     setTimeout(function(){
     $(".list-header").animate({opacity: 1}, 750);
   }, 3000);
   setTimeout(function(){
   $(".sbbtl-").animate({opacity: 1}, 750);
-}, 2500);
+}, 2250);
 }
 
 var isOpen = false;
 function w3_action() {
+
     if(!isOpen) {
-    $("#main").animate({'margin-left': '25%'}, 500);
-    // $("#main").animate({'width': '75%'}, 500);
-    // $(".header").animate({'margin-left': '25%'}, 500);
+    // $("#main").animate({'width': '75%'}, 250);
+    var ml = $(window).width()/4 - $('.bogo').width()/2 - 20;
+        $(".logo").animate({'margin-right': '50%'}, 250);
+        $(".logo").animate({'margin-left': ml}, 250);
+    $(".header").animate({'margin-left': '25%'}, 250);
 
         $("#openNav").addClass('txt-r-align');
 
-    $("#mySidebar").animate({width: '25%'}, 500);
+    $("#mySidebar").animate({width: '25%'}, 250);
 
-    $("#openNav").animate({'width': '25%'}, 500);
-    $('.logo').addClass('extend');
+    $("#openNav").animate({'width': '25%'}, 250);
     isOpen = true;
   } else {
-    $("#main").animate({'margin-left': '5%'}, 500);
-    // $("#main").animate({'width': '95%'}, 500);
-    // $(".header").animate({'margin-left': '0%'}, 500);
+      $(".logo").animate({'margin-right': '0%'}, 250);
+    // $("#main").animate({'width': '95%'}, 250);
+    $(".logo").animate({'padding-left': '12px'}, 250);
+    $(".logo").animate({'margin-left': '0px'}, 250);
+    $(".header").animate({'margin-left': '5%'}, 250);
+    // $(".logo").animate({'margin-left': 0}, 250);
     setTimeout(function(){
     $("#openNav").removeClass('txt-r-align');
   }, 400);
 
-    $("#mySidebar").animate({width: '5%'}, 500);
-    $("#openNav").animate({'width': '5%'}, 500);
-    $('.logo').removeClass('extend');
+
+
+    $("#mySidebar").animate({width: '5%'}, 250);
+    $("#openNav").animate({'width': '5%'}, 250);
+    // $('.logo').removeClass('extend');
     isOpen = false;
   }
+
+
+
 }
