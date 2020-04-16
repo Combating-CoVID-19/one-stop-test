2d0f376abd39f5c7ceb377f77d3ef73e53b0e078
ccans <55026208+ccans@users.noreply.github.com>
2020-04-16 16:45:12 -0500

    funky stuff

--- app/app.js
+++ app/app.js
@@ -1,41 +1,29 @@
-import React, {Component} from 'react'
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div className="dashboard container">
-        <div className="row">
-            <div className="col s12 m6"> </div>
-            <div className="col s12 m5 offset-m1"> </div>
-         </div>
-      </div>
-    )
+// Have automatically change on different widths
 
+
+
+var isOpen = false;
+
+function showMenu() {
+
+  var widtha = $('#dropdownMenuButton').width();
+
+  if(isOpen) {
+    $('.dropdown-menu').hide();
+    isOpen = false;
+  } else {
+    $('.dropdown-menu').show();
+    isOpen = true;
   }
 }
 
-function dropDown() {
-  if(droppedDown == false) {
-        $('.dropdown-info').stop();
-          curHeight =  $('.dropdown-info').height();
-          autoHeight = $(window).height(); - $('.header').height();
-        $('.dropdown-info').height(curHeight).animate({height: autoHeight}, 750);
-        $('body').addClass("unscroll");
-        $('.dropdown-info').show();
-        $('.dropdown-content').css("display", "block");
-        $('.header').addClass('dropdown');
-        droppedDown = true;
-    } else {
-      $('.dropdown-info').stop();
-      curHeight = $('.dropdown-info').height();
-      autoHeight = '0px';
-      $('.dropdown-info').height(curHeight).animate({height: autoHeight}, 750);
-      $('body').removeClass("unscroll");
-      setTimeout(function(){
-      $('.dropdown-content').css("display", "none");
-    }, 750);
-      droppedDown = false;
-      $('.header').removeClass("dropdown")
+window.onload = function() {
 
-    }
+var width = $(window).width();
+
+if(width < 767) {
+  console.log("ok boomer");
+  $('#dropdownMenuButton2').html("CAT");
+}
 }
