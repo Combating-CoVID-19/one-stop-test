0d4ce88de2c00b376efb50a5d38a3152940a0673
ccans <55026208+ccans@users.noreply.github.com>
2020-04-18 01:25:55 -0500

    few more theming fixes

--- app/app.js
+++ app/app.js
@@ -10,11 +10,13 @@ function showMenu() {
   var widtha = $('#dropdownMenuButton').width();
 
   if(isOpen) {
+    $('.block-o').stop();
     curWidth =  $('.block-o').width();
     $('.block-o').width(curWidth).animate({width:'100%'}, 750);
     // $('.dropdown-menu').hide();
     isOpen = false;
   } else {
+    $('.block-o').stop();
     curWidth =  $('.block-o').width();
     $('.block-o').width(curWidth).animate({width: '64px'}, 750);
     isOpen = true;
