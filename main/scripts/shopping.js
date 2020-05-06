a3afa82560126980f23e7953e6e1358b2335eb48
DhairyaG23 <dhairyag23@gmail.com>
2020-05-06 18:58:37 -0500

    updates to shopping

--- main/scripts/shopping.js
+++ main/scripts/shopping.js
@@ -1,10 +1,13 @@
 var bar = document.getElementById("search-bar")
-
+var amazon = document.getElementById('amazon')
 
 function search(){
     if(bar.value != null || bar.value != ' ' || bar.value != ''){
     console.log('Search query: '+bar.value)
-    }
+    amazon.innerHTML = 'Search Amazon for ' + bar.value;
+    $(amazon).attr('href', 'https://www.amazon.com/s?k='+bar.value)
+        
+}
 
 
 
